57deb8ffba3e5a58e5ab7c502bee1bd2828e3b75
habibowsasha-crypto <habibow.sasha@gmail.com>
2026-08-07 22:08:11 +0300

    Delete test_support directory

--- test_support/stubs/telethon/events/callbackquery.py
+++
@@ -1,2 +0,0 @@
-class CallbackQuery:
-    class Event: pass
--- test_support/stubs/telethon/tl/types.py
+++
@@ -1,5 +0,0 @@
-class InputPeerUser:
-    def __init__(self,user_id,access_hash): self.user_id=user_id; self.access_hash=access_hash
-class User: pass
-class Channel: pass
-class Chat: pass
